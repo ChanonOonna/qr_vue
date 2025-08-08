83e067ef0c006bea6d5814f67e01b096d428dad3
ChanonOonna <Chanon.oo@ku.th>
2025-08-08 15:31:36 +0700

    edit script download models

--- frontend-vue/scripts/download-models.js
+++ frontend-vue/scripts/download-models.js
@@ -1,6 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const https = require('https');
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const MODELS_DIR = path.join(__dirname, '../public/models');
 const FACE_API_BASE_URL = 'https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights';
